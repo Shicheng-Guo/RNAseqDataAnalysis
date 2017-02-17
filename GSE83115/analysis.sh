51583cccd459005dd54b733b5e20eb4d3e147e38
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-02-17 15:10:05 -0800

    Update analysis.sh

--- GSE83115/analysis.sh
+++ GSE83115/analysis.sh
@@ -1,1 +1,5 @@
 
+mkdir GSE83115
+cd GSE83115
+wget ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE83nnn/GSE83115/suppl/GSE83115_14_tissues_28_samples_156600_transcripts_FPKM.txt.gz
+gunzip GSE83115_14_tissues_28_samples_156600_transcripts_FPKM.txt.gz
